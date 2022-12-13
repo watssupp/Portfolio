dc68a1a9ee36765d89728f768aa80b9957f1b551
watssupp <jorgesolisruiz@gmal.com>
2022-12-13 16:08:38 -0600

    added colors to the root css

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/nav'
+import About from './components/about/About'
+import Experiance from './components/Experience/Experience'
+import Portfolio from './components/portfolio/portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/footer'
 
 const App = () => {
   return (
     <>
-    
+    <Header />
+    <Nav/>
+    <About/>
+    <Experiance/>
+    <Portfolio/>
+    <Contact/>
+    <Footer/>
     </>
   )
 }
--- react-portfolio/src/components/portfolio/portfolio.jsx
+++ react-portfolio/src/components/portfolio/portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./portfolio.css"
+import './portfolio.css'
 
 function portfolio() {
   return (
