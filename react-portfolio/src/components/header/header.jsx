3b0e68fb75ec859406b1376cd20daf6bcfe3ee6b
watssupp <jorgesolisruiz@gmal.com>
2022-12-14 12:07:49 -0600

    added # in index.js to connect the root

--- react-portfolio/src/components/header/header.jsx
+++ react-portfolio/src/components/header/header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 function header() {
   return (
-    <div>header</div>
+    <header>
+      <div classname=".container.header_container">
+        <h5>Hello I'm</h5>
+        <h1>Jorge Solis</h1>
+        <h5 classname="text-light">FullStack Developer</h5>
+      </div>
+    </header>
   )
 }
 
