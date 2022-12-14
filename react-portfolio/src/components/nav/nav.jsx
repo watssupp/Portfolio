1184bbf2f5de3abdc7be49288bf9b0a6ab725475
watssupp <jorgesolisruiz@gmal.com>
2022-12-14 14:42:18 -0600

    started making nav bar with icons

--- react-portfolio/src/components/nav/nav.jsx
+++ react-portfolio/src/components/nav/nav.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import  './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {BiMessageDetail} from 'react-icons/bi'
+
 
 function nav() {
   return (
-    <div>nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#contact"><BiMessageDetail/></a>
+    </nav>
   )
 }
 
