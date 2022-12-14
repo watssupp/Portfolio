ec6ed9b56f05530086e9184bc5aa3cf66c06527c
watssupp <jorgesolisruiz@gmal.com>
2022-12-14 12:48:22 -0600

    added contact/resume btns

--- react-portfolio/src/components/header/header.jsx
+++ react-portfolio/src/components/header/header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 function header() {
   return (
     <header>
-      <div classname=".container.header_container">
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Jorge Solis</h1>
-        <h5 classname="text-light">FullStack Developer</h5>
+        <h5 className="text-light">FullStack Developer</h5>
+        <CTA/>
       </div>
     </header>
   )
